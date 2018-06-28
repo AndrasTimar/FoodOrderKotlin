c81e800cf4f732b7213e8d8f7b61abb7c563e81d
AndrasTimar <bandi@almosttrio.com>
2018-06-28 23:21:48 +0200

    add FoodController with basic Mappings

---
+++ src/main/kotlin/me/andrastimar/FoodOrderKotlin/controller/FoodController.kt
@@ -0,0 +1,26 @@
+package me.andrastimar.FoodOrderKotlin.controller
+
+import me.andrastimar.FoodOrderKotlin.model.Food
+import me.andrastimar.FoodOrderKotlin.repository.FoodRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController("food")
+class FoodController {
+
+    @Autowired
+    lateinit var foodRepository: FoodRepository
+
+    @GetMapping
+    fun getFood() = foodRepository.findAll().take(10)
+
+    @PostMapping
+    fun createFood(@RequestBody food: Food): Food {
+        foodRepository.save(food)
+        return food
+    }
+}
