432cc511afd7ec52a2424fa9ef4179f3a1ec0db9
AndrasTimar <bandi@almosttrio.com>
2018-06-28 22:24:24 +0200

    initial commit

---
+++ src/main/kotlin/me/andrastimar/FoodOrderKotlin/FoodOrderKotlinApplication.kt
@@ -0,0 +1,11 @@
+package me.andrastimar.FoodOrderKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FoodOrderKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<FoodOrderKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/me/andrastimar/FoodOrderKotlin/FoodOrderKotlinApplicationTests.kt
@@ -0,0 +1,16 @@
+package me.andrastimar.FoodOrderKotlin
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class FoodOrderKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
